89d3267a065350117708941a7cb555a7a106bc67
LawrenceKhan <32395268+LawrenceKhan@users.noreply.github.com>
2017-10-06 09:57:22 -0400

    Conditional statements and more
    Conditional statements, unary and ternary statements

--- Class10/Class10.java
+++ Class10/Class10.java
@@ -16,9 +16,19 @@ public class Class10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*
+        ! is an unary operator so are
+        + in front of an value to indicate positive values like +10
+        + in front of an value to indicate negative values like +10
+        ++ increment
+        -- decrement
+        we have seen everything above so far but not the use of ! except for
+        comparing when not equal to something.
+        */
+        
         boolean value1  = true;
         boolean value2 = false;
-        //ways to compare
+        //ways to compare when they are not equal to each other
         if(value1!=value2){
             System.out.println("They are unrelated");
         }
@@ -29,7 +39,7 @@ public class Class10 {
         if(!value1!=value2){
             System.out.println("They are unrelated");
         }else{
-            System.out.println("They are no unrelated. Franks says they related");
+            System.out.println("They are not unrelated. Franks says they related");
         }
         //how to check boolean values
         if(value1==true){
@@ -38,20 +48,31 @@ public class Class10 {
             System.out.println("Value 1 false");
         }
         //you don't have to check with == for boolean
-        if(!value2){
+        if(!value2){ //!value2 is like saying value2 == false
             System.out.println("Value 2 false");
         }else{
             System.out.println("Value 2 true");
         }
         
         //comparing two strings Algorithmic workbench#9
-        String a = "khan";
-        String b = "lawrence";
+        //following
+        String a = "title1";
+        String b = "title2";
         if(a.equalsIgnoreCase(b)){
             System.out.println("They are equal to each other");
         }else{
             System.out.println("They are not equal to each other");
         }
+        /*Console output
+        *    They are not equal to each other
+        */
+        /*
+        * To compare two string to determine how they are ordered then we use the compare method.
+        * Compare method returns either -1 or 0 or 1
+        * -1 means a is left hand side of b or above b
+        * 0 means a and b are equal to eachother
+        * 1 means a is on the right hand side or below b
+        */
         System.out.println(a.compareTo(b));
         
         //ternary system
