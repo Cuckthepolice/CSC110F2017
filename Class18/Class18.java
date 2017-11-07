049e3db2f0685671fe9264f422678cfa999a0277
LawrenceKhan <32395268+LawrenceKhan@users.noreply.github.com>
2017-11-07 18:16:27 -0500

    Read Files

---
+++ Class18/Class18.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class18;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lawrence
+ */
+public class Class18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO code application logic here
+        
+        FileReader fileReader = new FileReader("/Users/Lawrence/Downloads/MSFT.csv");
+        Scanner reader = new Scanner(fileReader);
+        
+//        System.out.println(reader.nextLine());
+//        System.out.println(reader.nextLine());
+//        System.out.println(reader.nextLine());
+//        System.out.println(reader.nextLine());
+        
+        while(reader.hasNextLine()){
+            System.out.println(reader.nextLine());
+        }
+        
+        
+    }
+    
+}
